90e8769a7a61c15e991922a7ccb8e575eb3f64c8
levancongquan2 <95332031+levancongquan2@users.noreply.github.com>
2022-09-05 10:40:53 +0700

    commit bai th ngay 5 thang 11

---
+++ app/src/main/java/com/example/th_ngay5/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.th_ngay5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        button = (Button) findViewById (R.id.chuyentap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
